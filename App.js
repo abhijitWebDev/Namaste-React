1b5dc6fefdffa8c304df249add1a26106c01825b
Abhijit Mone <mone.abhijit@gmail.com>
2022-12-25 07:05:28 +0530

    Assignment commit

---
+++ App.js
@@ -0,0 +1,29 @@
+
+// const heading = document.createElement("h1");
+// heading.innerHTML="Namaste Javascript";
+// const root = document.getElementById("root");
+// root.appendChild(heading);
+
+const Heading1 = React.createElement(
+  'h1',
+  {
+    key: 'Heading1',
+  },
+  'Heading1'
+);
+const Heading2 = React.createElement(
+  'h1',
+  {
+    key: 'Heading2',
+  },
+  'Heading2'
+);
+
+const container = React.createElement("div", {id:"container"},[Heading1, Heading2] );
+
+
+console.log(container);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
+
+
