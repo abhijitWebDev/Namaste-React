0ec4bdfbbd6aadaccd4393bf1d9f5cfa31f5a04e
Abhijit Mone <mone.abhijit@gmail.com>
2023-01-09 22:49:07 +0530

    Assignment -3

--- App.js
+++ App.js
@@ -1,30 +1,47 @@
 import React from "react";
 import  ReactDOM  from "react-dom/client";
-// const heading = document.createElement("h1");
-// heading.innerHTML="Namaste Javascript";
-// const root = document.getElementById("root");
-// root.appendChild(heading);
 
-const Heading1 = React.createElement(
-  'h1',
-  {
-    key: 'Heading1',
-  },
-  'Heading1'
-);
-const Heading2 = React.createElement(
-  'h1',
-  {
-    key: 'Heading2',
-  },
-  'Heading2'
-);
 
-const container = React.createElement("div", {id:"container"},[Heading1, Heading2] );
+/**
+     Header
+        - Logo(Title)
+        - Nav Items(Right Side)
+        - Cart
+     Body 
+        - Search bar
+        - RestrauntList
+          - RestaurantCard (many cards)
+              - Image
+              - Name
+              - Rating
+              - Cusines
+     Footer
+      - links
+      - Copyright
+    
+    */
+
+
+// What is JSX
+const Title = () => (
+    <h1 id="title" key="h2">
+      Namaste React
+   </h1>
+   );
+
+const HeaderComponent = () => {
+  return (
+    <div>
+      <Title />
+      <h2>Namaste React functional component</h2>
+      <h2>This is a h2 tag</h2>
+    </div>
+  );
+};
 
 
 console.log(container);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
+root.render(<HeaderComponent />);
 
 
